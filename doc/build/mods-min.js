bcbdffe7cd364872e5f4c3184ae56253fadcd59a
弘树 <tiehang.lth@alibaba-inc.com>
2014-07-31 20:40:16 +0800

    sync doc

---
+++ doc/build/mods-min.js
@@ -0,0 +1,2 @@
+/*!build time : 2014-07-31 8:40:05 PM*/
+KISSY.config("modules",{"kg/auth/2.0.0/index":{requires:["node","json","base","promise","event","dom"]}});
---
+++ doc/build/mods.js
@@ -0,0 +1,3 @@
+KISSY.config('modules', {
+ 'kg/auth/2.0.0/index': { requires: ['node', 'json', 'base', 'promise', 'event', 'dom']} 
+});
