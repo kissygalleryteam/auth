3eeee611da44862e80799fcdfd77022e304b855e
弘树 <tiehang.lth@alibaba-inc.com>
2014-10-24 04:25:47 +0800

    sync daily branch 2.0.1

--- doc/build/mods-min.js
+++
@@ -1,2 +0,0 @@
-/*!build time : 2014-10-23 4:49:29 PM*/
-KISSY.config("modules",{"kg/auth/2.0.1/index":{requires:["node","json","base","promise","event","dom"]}});
--- doc/build/plugin/msgs/mods-min.js
+++
@@ -1,2 +0,0 @@
-/*!build time : 2014-10-23 4:49:29 PM*/
-KISSY.config("modules",{"kg/auth/2.0.1/plugin/msgs/msg":{requires:["base","node","xtemplate"]}});
