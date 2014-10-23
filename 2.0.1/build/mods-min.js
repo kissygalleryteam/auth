6bded390cb05f6a3d489fea59f0c44a56d039b01
弘树 <tiehang.lth@alibaba-inc.com>
2014-10-23 17:19:06 +0800

    sync daily version 2.0.1

--- 2.0.1/build/mods-min.js
+++ 2.0.1/build/mods-min.js
@@ -1,2 +1,2 @@
-/*!build time : 2014-07-31 8:40:05 PM*/
-KISSY.config("modules",{"kg/auth/2.0.0/index":{requires:["node","json","base","promise","event","dom"]}});
+/*!build time : 2014-10-23 4:49:29 PM*/
+KISSY.config("modules",{"kg/auth/2.0.1/index":{requires:["node","json","base","promise","event","dom"]}});
