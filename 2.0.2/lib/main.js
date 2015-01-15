731f5f28e6dd67dac42ddfa846451904d3a78163
kissyteam <tiehang.lth@taobao.com>
2015-01-15 16:53:14 +0800

    sync daily version 2.0.2

---
+++ 2.0.2/lib/main.js
@@ -0,0 +1,21 @@
+/**
+ * @fileoverview
+ * @author
+ **/
+KISSY.add(function (S, Node,Base) {
+    var EMPTY = '';
+    var $ = Node.all;
+
+    return Base.extend({
+        initializer:function(){
+
+        }
+    },{
+        ATTRS:{
+
+        }
+    })
+}, {requires:['node', 'base']});
+
+
+
