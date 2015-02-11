a8abf8c1be994c194430432713bf81136bcdcedb
kissyteam <tiehang.lth@taobao.com>
2015-02-11 14:56:19 +0800

    sync daily version 2.0.6

---
+++ 2.0.6/build/mods-min.js
@@ -0,0 +1,2 @@
+/*!build time : 2015-02-11 2:54:52 PM*/
+KISSY.config("modules",{"kg/auth/2.0.6/index":{requires:["node","json","base","promise","event","dom"]}});
---
+++ 2.0.6/build/plugin/msgs/mods-min.js
@@ -0,0 +1,2 @@
+/*!build time : 2015-02-11 2:54:52 PM*/
+KISSY.config("modules",{"kg/auth/2.0.6/plugin/msgs/msg":{requires:["base","node","xtemplate"]}});
